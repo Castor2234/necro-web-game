a486519d6725e486092a23efc834e53e9073ed65
Castor2234 <castor2234@gmail.com>
2026-07-27 15:28:32 +0300

    deleted old assets, added new assets, changed game config

--- src/game/main.ts
+++ src/game/main.ts
@@ -9,10 +9,22 @@ import { Preloader } from './scenes/Preloader';
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 640,
+    height: 360,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#000000',
+
+    render: { 
+        pixelArt: true, 
+        antialias: false,
+        roundPixels: true 
+    },
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    
     scene: [
         Boot,
         Preloader,
