a01583a34bcd962380a0645f1640872602412d9d
Castor2234 <castor2234@gmail.com>
2026-07-28 10:45:01 +0300

    Начинаю переносить игру

--- src/game/scenes/2MainMenu.ts
+++ src/game/scenes/2MainMenu.ts
@@ -1,13 +1,11 @@
-import { GameObjects, Scene } from 'phaser';
+import { Scene } from 'phaser';
 
 import { EventBus } from '../EventBus';
 
 export class MainMenu extends Scene
 {
-    background: GameObjects.Image;
-    forest: GameObjects.Image;
-    title: GameObjects.Text;
-    forestTween: Phaser.Tweens.Tween | null;
+    background: Phaser.GameObjects.Image;
+    title: Phaser.GameObjects.Text;
 
     constructor ()
     {
@@ -17,8 +15,7 @@ export class MainMenu extends Scene
     create ()
     {
         this.background = this.add.image(320, 180, 'background');
-
-        this.forest = this.add.image(512, 300, 'forest').setDepth(100);
+        this.background.setAlpha(0.5);
 
         this.title = this.add.text(320, 100, 'Главное меню', {
             fontFamily: 'sans-serif', fontSize: 40, color: '#faf8f8',
--- src/game/scenes/3Game.ts
+++ src/game/scenes/3Game.ts
@@ -3,28 +3,59 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
-    camera: Phaser.Cameras.Scene2D.Camera;
+    // Scene setup
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
+    camera: Phaser.Cameras.Scene2D.Camera;
+    
+    // Images
+    forest: Phaser.GameObjects.Image;
+    village: Phaser.GameObjects.Image;
+
+    // Starting values
+    private ratSpeed = 400;
+    private v1pw = 50;
+
+    private ratAmount = 0;
+    private corpseAmount = 2;
+    private lootMulti = 1;
+
 
     constructor ()
     {
         super('Game');
     }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.zoomTo(2,2000)
+    public init (data: number): void {
+        // this.score = data.score || 0;
+    }
 
-        this.background = this.add.image(320, 180, 'background');
-        this.background.setAlpha(0.5);
+    public create (): void {
+        // Bg 
+        this.background = this.add.image(320, 180, 'background').setDepth(-1);
 
-        this.gameText = this.add.text(320, 100, 'Игра', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 4,
+        // Scene title
+        this.gameText = this.add.text(220, 50, 'Игра', {
+            fontFamily: 'Arial Black', fontSize: 32, color: '#b98ba0',
+            stroke: '#000000', strokeThickness: 2,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
+
+        // Camera
+        this.camera = this.cameras.main;
+        this.camera.setZoom(1.3)
+        this.camera.setScroll(-70,-10)
+
+        // Forest
+        this.forest = this.add.image(100, 160, 'forest').setScale(2);
+
+        // Village
+        this.village = this.add.image(350, 160, 'village').setScale(2);
+
+        
+
+
+
 
         EventBus.emit('current-scene-ready', this);
     }
