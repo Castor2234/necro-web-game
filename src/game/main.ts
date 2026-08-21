ea899499df557f615f90650e3780c95054b47604
Castor2234 <castor2234@gmail.com>
2026-08-21 17:05:38 +0300

    Убрал debug с физики phaser

--- src/game/main.ts
+++ src/game/main.ts
@@ -16,7 +16,7 @@ const config: Phaser.Types.Core.GameConfig = {
   backgroundColor: '#000000',
   physics: {
     default: 'arcade',
-    arcade: { gravity: { x: 0, y: 0 }, debug: true },
+    arcade: { gravity: { x: 0, y: 0 }, debug: false },
   },
 
   render: {
