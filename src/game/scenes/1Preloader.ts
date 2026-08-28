bc182aea1509d39a94b17f5487395ba6b13f15eb
Castor2234 <castor2234@gmail.com>
2026-08-28 03:52:04 +0300

    Фикс прогресс бара загрузки

--- src/game/scenes/1Preloader.ts
+++ src/game/scenes/1Preloader.ts
@@ -15,12 +15,11 @@ export class Preloader extends Scene {
     this.add.rectangle(320, 180, 240, 32).setStrokeStyle(1, 0xffffff);
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+    const bar = this.add.rectangle(200, 180, 4, 28, 0xffffff);
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-    this.load.on('progress', (progress: number) => {
-      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-      bar.width = 4 + 236 * progress;
+    this.load.on('progress', (p: number) => {
+      bar.width = 236 * p;
     });
   }
 
